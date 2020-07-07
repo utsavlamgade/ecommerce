70c5bd77c0f560675addf218e40c728a7f373b94
utsavlamgade <54902689+utsavlamgade@users.noreply.github.com>
2020-07-07 14:24:47 -0400

    add everything

---
+++ store/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
+def home(request, category_slug=None):
+    category_page = None
+    products = None
+    if category_slug!=None:
+        category_page = get_object_or_404(Category,slug=category_slug)
+        products = Product.objects.filter(category=category_page, available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render(request, 'home.html', {'category': category_page, 'products': products})
+
+
+def productPage(request):
+    return render(request, 'product.html')
+# Create your views here.
